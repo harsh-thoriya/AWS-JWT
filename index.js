78f68c41fb884b688fb5b70be67dc48cc7bd1490
harsh-thoriya <harsh.thoriya@bacancy.com>
2021-03-05 16:50:20 +0530

    reset password and forget password options with mail facility included using jwt

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser')
 const indexRouter = require('./src/routers/index.js')
 const cookieParser = require('cookie-parser')
 const jsonWebToken = require('jsonwebtoken')
+const AWS = require('aws-sdk')
+const uuid = require('uuid')
 //const jsonwt = require('express-jwt')
 
 mongoose.connect('mongodb://localhost/AWS&JWT')
@@ -15,12 +17,33 @@ app.listen(3000,()=>{
     console.log("server is up on port 3000")
 })
 
-
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
 app.use(bodyParser.json());
 app.use(cookieParser())
 //app.use(jsonwt({secret:'abcd',algorithms:['HS256'],getToken: req => req.cookies.token}))
 app.use(express.static(path.join(__dirname, 'public','html')));
+app.use(express.static(path.join(__dirname, 'profilePics')));
+
+
+
+//const storage = multer.memoryStorage({
+//    destination: function(req, file, callback) {
+//        callback(null, '')
+//    }
+//})
+//  
+//  const filefilter = (req,file,callback)=>{
+//    if(file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg'){
+//      callback(null,true)
+//    }
+//    else{
+//      callback(null,false)
+//    }
+//  }
+//  
+//app.use(multer({storage : storage, filefilter : filefilter}).single('profilePic'))
+
 app.use(indexRouter)
--- src/routers/index.js
+++ src/routers/index.js
@@ -6,6 +6,9 @@ const CryptoJs = require('crypto-js')
 const userModel = require('../Models/users.js');
 const auth = require('../middleware/auth.js')
 const Cryptr = require('cryptr');
+const AWS = require('aws-sdk')
+const uuid = require('uuid')
+const nodemailer = require('nodemailer')
 
 const router = express.Router();
 
@@ -50,6 +53,48 @@ let generateToken = async function(user){
     return encryptedToken
 }
 
+router.post('/forgotPassword',async (req,res,next)=>{
+    
+    const email = req.body.email
+
+    const user = await userModel.findOne({email:email})
+    
+    const token = await generateToken(user)
+
+    try{
+
+        let mailTransporter = nodemailer.createTransport({ 
+            service: 'gmail', 
+            auth: { 
+                user: 'userdummy.dum@gmail.com', 
+                pass: '123$567*90'
+            } 
+        }); 
+          
+        let mailDetails = { 
+            from: 'userdummy.dum@gmail.com', 
+            to: req.body.email, 
+            subject: 'Forgot Password Link', 
+            text: 'http://127.0.0.1:3000/forgotPassword/token?jwt='+token
+        }; 
+          
+        mailTransporter.sendMail(mailDetails, function(err, data) { 
+            if(err) { 
+                console.log(err); 
+            } else { 
+                console.log('Email sent successfully'); 
+            } 
+        }); 
+    
+        res.send("Link for resetting password shared with you in email you registered with. Check your mail.")
+    }
+    catch(e){
+        res.send("error while signing up , try again after some time")
+    }
+
+    
+})
+
 router.post('/',async (req,res,next)=>{
     try {
         const user = await userModel.findOne({username: req.body.username,password:req.body.password})
@@ -64,12 +109,71 @@ router.post('/',async (req,res,next)=>{
     }
 })
 
+//const S3 = new AWS.S3({
+//    accessKeyId : "ASIARLOHTBV64XOIRUC4",
+//    secretAccessKey: "rCTQhA3/C0cPFZnaFRXJfO/+8bhSrFDNVEoyf+YT"
+//})
+
+//router.post('/upload',(req,res)=>{
+//    const params = {
+//        Bucket: 'prcts3',
+//        Key : 'random111.jpg',
+//        Body: req.file.buffer
+//    }
+//
+//    S3.upload(params,(err,data)=>{
+//        if(err){
+//            return res.send(err)
+//        }
+//        else{
+//            console.log(data)
+//            res.send("aaaaaaaaaa")
+//        }
+//    })
+//})
+//
 router.post('/signup',async (req,res,next)=>{
 
-    const user = new userModel(req.body)
-    await user.save()
+    
+    //console.log(req.file)
+    try{
+        const user = new userModel({
+            username : req.body.username,
+            email: req.body.email,
+            password : req.body.password
+        })
+        await user.save()
 
-    res.redirect('/')
+        let mailTransporter = nodemailer.createTransport({ 
+            service: 'gmail', 
+            auth: { 
+                user: 'userdummy.dum@gmail.com', 
+                pass: '123$567*90'
+            } 
+        }); 
+          
+        let mailDetails = { 
+            from: 'userdummy.dum@gmail.com', 
+            to: user.email, 
+            subject: 'Sign Up Confirmed', 
+            text: 'Congratulations! You can now login'
+        }; 
+          
+        mailTransporter.sendMail(mailDetails, function(err, data) { 
+            if(err) { 
+                console.log(err); 
+            } else { 
+                console.log('Email sent successfully'); 
+            } 
+        }); 
+    
+        res.redirect('/')
+    }
+    catch(e){
+        res.send("error while signing up , try again after some time")
+    }
+
+    
 })
 
 router.get('/image',auth,(req,res,next)=>{
@@ -84,12 +188,64 @@ router.get('/logout',auth,async (req,res,next)=>{
         })
         console.log(req.user)
         await req.user.save()
-
+        res.clearCookie("token");
         res.redirect('/')
     } catch (e) {
         res.status(500).send('Error occured during database operation')
     }
+})
 
+router.get('/forgotPassword',(req,res,next)=>{
+    res.sendFile(pathToPublic+'/forgotpassword.html')
+})
+
+router.get('/forgotPassword/token',async (req,res,next)=>{
+    console.log(req.query.jwt)
+    try {
+        const decryptedToken = cryptr.decrypt(req.query.jwt);
+        const decoded = jsonWebTokens.verify(decryptedToken,'abcd')
+        const user = await userModel.findOne({ _id: decoded._id, 'jwts.token': req.query.jwt })
+
+        if (!user) {
+            console.log("inside if")
+            throw new Error()
+        }
+        
+        //req.token = req.cookies.token
+        req.user = user
+        res.cookie('token',req.query.jwt,{httpOnly:true})
+        res.sendFile(pathToPublic+'/forgotpasswordform.html')
+    } catch (e) {
+        res.send("Invalid URL")
+    }
+})
+
+router.post('/forgotPassword/token',async (req,res,next)=>{
+    console.log(req.cookies.token)
+    try {
+        const decryptedToken = cryptr.decrypt(req.cookies.token);
+        const decoded = jsonWebTokens.verify(decryptedToken,'abcd')
+        const user = await userModel.updateOne({ _id: decoded._id},{$set : {password : req.body.newPassword},$unset : {jwts : []}})
+        res.clearCookie("token");
+        res.send("Successfully updated password. Now you can login with your new password")
+    } catch (e) {
+        res.send("Invalid URL")
+    }
+})
+
+router.get('/resetPassword',auth,(req,res,next)=>{
+    res.sendFile(pathToPublic+'/resetpassword.html')
+})
+
+router.post('/resetPassword',auth,async (req,res,next)=>{
+    const user = await userModel.findOne({_id:req.user._id, password : req.body.currentPassword})
+    if(!user){
+        return res.send("incorrect password")
+    }
+    const updatedUsers = await userModel.updateOne({_id:user._id},{password: req.body.newPassword})
+    
+    res.clearCookie("token")
+    res.send("password updated , now login again")
 })
 
 module.exports = router
